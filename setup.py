65a71274c83da4ba5bb4b058ae2678f124fc6d49
George Wood <george.wood@55places.com>
2020-08-10 17:02:56 -0500

    DE-155/166 | Dev Packages and Upload Exceptions (#17)
    * DE-155/166 | Dev Packages and Upload Exceptions
    
    * Update pandavro - hooray open source
    
    * rename var

--- setup.py
+++ setup.py
@@ -46,8 +46,11 @@ class UploadCommand(Command):
                   sys.executable))
 
         self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/* '
-                  '--repository-url http://pypi.neighborhoods.com/simple/')
+        returned_error = os.system(
+            'twine upload dist/* '
+            '--repository-url http://pypi.neighborhoods.com/simple/')
+        if returned_error:
+            raise ValueError('Pushing to PyPi failed.as')
 
         self.status('Pushing git tags…')
         os.system('git tag v{0}'.format(about["__version__"]))
