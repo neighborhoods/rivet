7b8a7fa1f9d055fd317523b042833c64df40e8e3
George Wood <george.wood@55places.com>
2021-04-19 18:11:16 -0500

    added tests for compressed files

--- test/test_write.py
+++ test/test_write.py
@@ -21,6 +21,148 @@ def test_write_csv(setup_bucket_wo_contents, test_bucket,
             assert df.equals(test_df)
 
 
+def test_write_csv_gz(setup_bucket_wo_contents, test_bucket,
+                      test_df, test_df_keys):
+    """
+    Tests that writing files stored as a gzip-compressed CSV works properly
+    """
+    s3 = boto3.client('s3')
+
+    for key in test_df_keys['csv.gz']:
+        write(test_df, key, test_bucket)
+
+        with NamedTemporaryFile(suffix=".csv.gz") as tmpfile:
+            s3.download_file(test_bucket, key, tmpfile.name)
+            df = pd.read_csv(tmpfile.name)
+            assert df.equals(test_df)
+
+
+def test_write_csv_zip(setup_bucket_wo_contents, test_bucket,
+                       test_df, test_df_keys):
+    """
+    Tests that writing files stored as a zip-compressed CSV works properly
+    """
+    s3 = boto3.client('s3')
+
+    for key in test_df_keys['csv.zip']:
+        write(test_df, key, test_bucket)
+
+        with NamedTemporaryFile(suffix=".csv.zip") as tmpfile:
+            s3.download_file(test_bucket, key, tmpfile.name)
+            df = pd.read_csv(tmpfile.name)
+            assert df.equals(test_df)
+
+
+def test_write_csv_bz2(setup_bucket_wo_contents, test_bucket,
+                       test_df, test_df_keys):
+    """
+    Tests that writing files stored as a bzip2-compressed CSV works properly
+    """
+    s3 = boto3.client('s3')
+
+    for key in test_df_keys['csv.bz2']:
+        write(test_df, key, test_bucket)
+
+        with NamedTemporaryFile(suffix=".csv.bz2") as tmpfile:
+            s3.download_file(test_bucket, key, tmpfile.name)
+            df = pd.read_csv(tmpfile.name)
+            assert df.equals(test_df)
+
+
+def test_write_csv_xz(setup_bucket_wo_contents, test_bucket,
+                      test_df, test_df_keys):
+    """
+    Tests that writing files stored as an xz-compressed CSV works properly
+    """
+    s3 = boto3.client('s3')
+
+    for key in test_df_keys['csv.xz']:
+        write(test_df, key, test_bucket)
+
+        with NamedTemporaryFile(suffix=".csv.xz") as tmpfile:
+            s3.download_file(test_bucket, key, tmpfile.name)
+            df = pd.read_csv(tmpfile.name)
+            assert df.equals(test_df)
+
+
+def test_write_psv(setup_bucket_wo_contents, test_bucket,
+                   test_df, test_df_keys):
+    """Tests that writing files stored as a PSV works properly"""
+    s3 = boto3.client('s3')
+
+    for key in test_df_keys['psv']:
+        write(test_df, key, test_bucket)
+
+        with NamedTemporaryFile() as tmpfile:
+            s3.download_file(test_bucket, key, tmpfile.name)
+            df = pd.read_csv(tmpfile.name, sep='|')
+            assert df.equals(test_df)
+
+
+def test_write_psv_gz(setup_bucket_wo_contents, test_bucket,
+                      test_df, test_df_keys):
+    """
+    Tests that writing files stored as a gzip-compressed PSV works properly
+    """
+    s3 = boto3.client('s3')
+
+    for key in test_df_keys['psv.gz']:
+        write(test_df, key, test_bucket)
+
+        with NamedTemporaryFile(suffix=".psv.gz") as tmpfile:
+            s3.download_file(test_bucket, key, tmpfile.name)
+            df = pd.read_csv(tmpfile.name, sep='|')
+            assert df.equals(test_df)
+
+
+def test_write_psv_zip(setup_bucket_wo_contents, test_bucket,
+                       test_df, test_df_keys):
+    """
+    Tests that writing files stored as a zip-compressed PSV works properly
+    """
+    s3 = boto3.client('s3')
+
+    for key in test_df_keys['psv.zip']:
+        write(test_df, key, test_bucket)
+
+        with NamedTemporaryFile(suffix=".psv.zip") as tmpfile:
+            s3.download_file(test_bucket, key, tmpfile.name)
+            df = pd.read_csv(tmpfile.name, sep='|')
+            assert df.equals(test_df)
+
+
+def test_write_psv_bz2(setup_bucket_wo_contents, test_bucket,
+                       test_df, test_df_keys):
+    """
+    Tests that writing files stored as a bzip2-compressed PSV works properly
+    """
+    s3 = boto3.client('s3')
+
+    for key in test_df_keys['psv.bz2']:
+        write(test_df, key, test_bucket)
+
+        with NamedTemporaryFile(suffix=".psv.bz2") as tmpfile:
+            s3.download_file(test_bucket, key, tmpfile.name)
+            df = pd.read_csv(tmpfile.name, sep='|')
+            assert df.equals(test_df)
+
+
+def test_write_psv_xz(setup_bucket_wo_contents, test_bucket,
+                      test_df, test_df_keys):
+    """
+    Tests that writing files stored as an xz-compressed PSV works properly
+    """
+    s3 = boto3.client('s3')
+
+    for key in test_df_keys['psv.xz']:
+        write(test_df, key, test_bucket)
+
+        with NamedTemporaryFile(suffix=".psv.xz") as tmpfile:
+            s3.download_file(test_bucket, key, tmpfile.name)
+            df = pd.read_csv(tmpfile.name, sep='|')
+            assert df.equals(test_df)
+
+
 def test_write_feather(setup_bucket_wo_contents, test_bucket,
                        test_df, test_df_keys):
     """Tests that writing files stored as feather works properly"""
